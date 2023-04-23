d7b8246cb19cdc4973fa2d26ebd1630f33301311
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-04-23 00:23:53 -0500

    it looks semi decent now

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GridPart from "./components/GridPart.js";
 function App() {
   return (
     <div className="App">
+      <div id="title" className="title-font">EISENHOWER MATRIX</div>
       <div id = "mid-container">
         <div id = "mid-grid">
           <GridPart id="top-left-grid"/>
--- src/components/GridPart.js
+++ src/components/GridPart.js
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
 import "../scss/GridPart.scss"
+
+const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+function TaskInput({pos, index})
+{
+    const [taskText, setTaskText] = useState(`Task ${index}`)
+    return <div id={pos+" "+index} className="TaskInput">
+        <div className="outside-index">{index}</div>
+        <input type="text" className="task-title" value={taskText} onChange={(e) => setTaskText(e.target.value)}></input>
+    </div>
+}
 
 function GridPart({id})
 {
     return <div id={id}>
-
+        <div className="sugested-action">Do</div>
+        {ids.map((index) => <TaskInput pos={id} index = {index} key={index}/>)}
     </div>
 }
 

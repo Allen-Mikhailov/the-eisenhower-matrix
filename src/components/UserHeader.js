f570002c33c6d10f9c2d9e82e0ac46de9b847643
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2023-04-29 19:15:15 -0500

    Added dispay name/ google auth working properly

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,15 +1,34 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import "../scss/UserHeader.scss"
 
-function HasUser()
+
+const auth = getAuth();
+
+function HasUser({user})
 {
     return <div className="user-header">
-        
+        <div onClick={() => auth.signOut()} className="sign-out-button sub1-font">Sign Out</div>
+        <div className="user-display-name sub1-font">{user.displayName}</div>
+    </div>
+}
+
+function NoUser()
+{
+
+    function googleSignIn()
+    {
+        const provider = new GoogleAuthProvider()
+        signInWithPopup(auth, provider)
+    }
+
+    return <div className="user-header">
+        <div onClick={googleSignIn} className="sign-in-button sub1-font">Sign In</div>
     </div>
 }
 
 function UserHeader({user})
 {       
-    return user? <HasUser user={user}/>:<></>
+    return user? <HasUser user={user}/>:<NoUser/>
 }
 
 export default UserHeader
